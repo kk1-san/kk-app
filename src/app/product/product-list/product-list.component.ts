c9d75194c4d40f3440a68676ff573b7cf955c3c8
kkkff97 <planet.kkff97@gmail.com>
2023-05-18 20:38:40 +0900

    module追加

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent {
+
+products: any = [1, 2, 3, 4]
 
 }
