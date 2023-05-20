0a82659d4248e96ebdb2b3f23ac8329c1060c283
kkkff97 <planet.kkff97@gmail.com>
2023-05-20 10:37:49 +0900

    商品ページ作り込み

---
+++ src/app/product/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+    {
+      name: 'Phone XL',
+      price: 799,
+      description: 'A large phone with one of the best screens'
+    },
+    {
+      name: 'Phone Standard',
+      price: 599,
+      description: 'A standard phone with one of the best screens'
+    },
+    {
+      name: 'Phone Mini',
+      price: 499,
+      description: 'A mini phone with one of the best cameras'
+    },
+    {
+      name: 'Phone Special',
+      price: 999,
+      description: 'A special phone with one of the best screens'
+    }
+];
