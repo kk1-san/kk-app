876aeaf3e98a51c90b9497b59f88e8df609b4725
kkkff97 <planet.kkff97@gmail.com>
2023-05-29 20:04:29 +0900

    難航したエンドポイント作成成功！

--- server/model/product.js
+++ server/model/product.js
@@ -17,3 +17,4 @@ const ProductSchema = new Schema({
 })
 
 module.exports = mongoose.model('Product', ProductSchema)
+
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,10 +1,18 @@
 const express = require('express')
-const router = express.Router()
-const Product = require('../model/product')
-
-router.get('', async function(req, res) {
+const productRoutes = express.Router()
+const Product =require('../model/product')
+ 
+productRoutes.get('', async function(req, res) {
     foundProducts = await Product.find({})
     res.json(foundProducts)
 })
 
-module.exports = router
+productRoutes.get('/:productId', async function(_req, res) {
+    const productId = _req.params.productId
+    foundProducts = await Product.findById(productId)
+    res.json(foundProducts)
+})
+ 
+module.exports = productRoutes
+
+
